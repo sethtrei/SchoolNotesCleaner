a67fe1a31e271aef710497841cb9698d39809686
BuildTools <seth.treiman@gmail.com>
2017-11-25 16:42:20 -0500

    add comments and get rid of unused node/jquery stuff

--- source/background.js
+++ source/background.js
@@ -1,8 +1,8 @@
-
+//Remove stupid header elements
 $( "img" ).remove();
 $("div[id=header-internal]").remove();
 $("div[class=print-view-btn]").remove();
+//Set margins and remove gray text
 $("*").css("margin-left","auto");
 $("*").css("margin-right","auto");
 $('span[style*="color: rgb(153, 153, 153)"]').remove();
-$("*").css("display","block");
