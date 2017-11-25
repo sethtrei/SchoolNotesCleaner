eee6ddd189e4963e0c9e4d2d9f16fef31adeb8d7
BuildTools <seth.treiman@gmail.com>
2017-11-25 12:55:45 -0500

    Jquery works, removes gray text. Left to do: remove spacing

---
+++ source/background.js
@@ -0,0 +1,8 @@
+
+$( "img" ).remove();
+$("div[id=header-internal]").remove();
+$("div[class=print-view-btn]").remove();
+$("*").css("margin-left","auto");
+$("*").css("margin-right","auto");
+$('span[style*="color: rgb(153, 153, 153)"]').remove();
+$("*").css("display","block");
